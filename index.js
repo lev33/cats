d1a8ab01c04cc57ae04338c54030f382849f37c0
lev33 <48180783+lev33@users.noreply.github.com>
2022-12-15 23:05:18 +0300

    add deleteCat

--- index.js
+++ index.js
@@ -7,21 +7,50 @@ const app = async () => {
 
   const $wr = document.querySelector('[data-wr]');
 
+  const ACTIONS = {
+    DETAILS: 'details',
+    DELETE: 'delete',
+  };
+
+  const deleteCat = async ($catWr) => {
+    const { catId } = $catWr.dataset;
+    const url = `https://cats.petiteweb.dev/api/single/lev33/delete/${catId}`;
+    try {
+      const response = await fetch(url, {
+        method: 'DELETE',
+      });
+      if (response.status === 200) {
+        $catWr.remove();
+      } else {
+        throw new Error('Кот не удалился');
+      }
+    } catch (error) {
+      console.error('Ошибка:', error);
+    }
+  };
+
   const renderCats = () => {
     const getCatHTML = (cat) => `
-              <div class="card mb-4 mx-2" style="width: 18rem;">
+              <div data-cat-id="${cat.id}" class="card mb-4 mx-2" style="width: 18rem;">
                    <img src="${cat.image}" class="card-img-top" alt="${cat.name}">
                    <div class="card-body">
                       <h5 class="card-title">${cat.name}</h5>
                       <p class="card-text">${cat.description}.</p>
-                       <a href="#" class="btn btn-primary">Edit</a>
-                       <button id="${cat.id}" type="button" class="btn btn-danger">Delete</button>
+                       <button data-actions="${ACTIONS.DETAILS}" type="button" class="btn btn-primary">Details</button>
+                       <button data-actions="${ACTIONS.DELETE}" type="button" class="btn btn-danger">Delete</button>
                   </div>
               </div>
               `;
 
     state.cats.forEach((cat) => {
       $wr.insertAdjacentHTML('afterbegin', getCatHTML(cat));
+    });
+
+    $wr.addEventListener('click', (e) => {
+      if (e.target.dataset.actions === ACTIONS.DELETE) {
+        const $catWr = e.target.closest('[data-cat-id]');
+        deleteCat($catWr);
+      }
     });
   };
 
