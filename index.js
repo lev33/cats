60b3c73e2795579c0547a6ad5fb90f46f7d3c621
lev33 <48180783+lev33@users.noreply.github.com>
2022-12-17 19:25:47 +0300

    add new cat modal

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const app = async () => {
   };
 
   const $wr = document.querySelector('[data-wr]');
+  const $modalWr = document.querySelector('[data-modalWr]');
+  const $modalContent = document.querySelector('[data-modalContent]');
+  const $catCreateFormTemplate = document.getElementById('createCatForm');
 
   const ACTIONS = {
     DETAILS: 'details',
     DELETE: 'delete',
   };
+
+  const CREATE_FORM_LS_KEY = 'CREATE_FORM_LS_KEY';
 
   const deleteCat = async ($catWr) => {
     const { catId } = $catWr.dataset;
@@ -29,19 +34,19 @@ const app = async () => {
     }
   };
 
-  const renderCats = () => {
-    const getCatHTML = (cat) => `
-              <div data-cat-id="${cat.id}" class="card mb-4 mx-2" style="width: 18rem;">
-                   <img src="${cat.image}" class="card-img-top" alt="${cat.name}">
-                   <div class="card-body">
-                      <h5 class="card-title">${cat.name}</h5>
-                      <p class="card-text">${cat.description}.</p>
-                       <button data-actions="${ACTIONS.DETAILS}" type="button" class="btn btn-primary">Details</button>
-                       <button data-actions="${ACTIONS.DELETE}" type="button" class="btn btn-danger">Delete</button>
-                  </div>
-              </div>
-              `;
+  const getCatHTML = (cat) => `
+  <div data-cat-id="${cat.id}" class="card mb-4 mx-2" style="width: 18rem;">
+       <img src="${cat.image}" class="card-img-top" alt="${cat.name}">
+       <div class="card-body">
+          <h5 class="card-title">${cat.name}</h5>
+          <p class="card-text">${cat.description}.</p>
+           <button data-actions="${ACTIONS.DETAILS}" type="button" class="btn btn-primary">Details</button>
+           <button data-actions="${ACTIONS.DELETE}" type="button" class="btn btn-danger">Delete</button>
+      </div>
+  </div>
+  `;
 
+  const renderCats = () => {
     state.cats.forEach((cat) => {
       $wr.insertAdjacentHTML('afterbegin', getCatHTML(cat));
     });
@@ -55,6 +60,90 @@ const app = async () => {
   };
 
   renderCats();
+
+  const formatCreateFormData = (formDataObject) => ({
+    ...formDataObject,
+    id: +formDataObject.id,
+    rate: +formDataObject.rate,
+    age: +formDataObject.age,
+    favorite: !!formDataObject.favorite,
+  });
+
+  const clickModalWrHandler = (e) => {
+    if (e.target === $modalWr) {
+      $modalWr.classList.add('hidden');
+      $modalWr.removeEventListener('click', clickModalWrHandler);
+      $modalContent.innerHTML = '';
+    }
+  };
+
+  const openModalHandler = (e) => {
+    const targetModalName = e.target.dataset.openmodal;
+
+    if (targetModalName === 'createCat') {
+      $modalWr.classList.remove('hidden');
+      $modalWr.addEventListener('click', clickModalWrHandler);
+
+      const cloneCatCreateForm = $catCreateFormTemplate.content.cloneNode(true);
+      $modalContent.appendChild(cloneCatCreateForm);
+
+      const $createCatForm = document.forms.createCatForm;
+      const dataFromLS = localStorage.getItem(CREATE_FORM_LS_KEY);
+      const preparedDataFromLS = dataFromLS && JSON.parse(dataFromLS);
+      if (preparedDataFromLS) {
+        Object.keys(preparedDataFromLS).forEach((key) => {
+          $createCatForm[key].value = preparedDataFromLS[key];
+        });
+      }
+
+      $createCatForm.addEventListener('submit', (submitEvent) => {
+        submitEvent.preventDefault();
+
+        const formDataObject = formatCreateFormData(
+          Object.fromEntries(new FormData(submitEvent.target).entries()),
+        );
+
+        fetch('https://cats.petiteweb.dev/api/single/lev33/add/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formDataObject),
+        }).then((res) => {
+          if (res.status === 200) {
+            $modalWr.classList.add('hidden');
+            $modalWr.removeEventListener('click', clickModalWrHandler);
+            $modalContent.innerHTML = '';
+            localStorage.removeItem(CREATE_FORM_LS_KEY);
+            state.cats.push(formDataObject);
+            $wr.insertAdjacentHTML(
+              'afterbegin',
+              getCatHTML(formDataObject),
+            );
+          } else {
+            throw Error('Ошибка при создании кота');
+          }
+        }).catch(alert);
+      });
+
+      $createCatForm.addEventListener('change', () => {
+        const formattedData = formatCreateFormData(
+          Object.fromEntries(new FormData($createCatForm).entries()),
+        );
+        localStorage.setItem(CREATE_FORM_LS_KEY, JSON.stringify(formattedData));
+      });
+    }
+  };
+
+  document.addEventListener('click', openModalHandler);
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      $modalWr.classList.add('hidden');
+      $modalWr.removeEventListener('click', clickModalWrHandler);
+      $modalContent.innerHTML = '';
+    }
+  });
 };
 
 app();
